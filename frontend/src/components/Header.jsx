3ab4354a6a0a071c939ed1290232d98a10b798a9
jatobro <jakob.tossebro@gmail.com>
2023-02-09 12:08:37 +0100

    frontend authentication completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,46 @@
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {FaSignInAlt, FaSignOutAlt} from 'react-icons/fa'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 
 function Header() {
-  return (
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
+    return (
     <header className='header'>
         <div className='logo'>
-            <Link to='/'>Sheetr</Link>
+            <Link to='/'>Disco & Duty</Link>
         </div>
         <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt /> Login 
-                </Link>
-            </li> 
+            {user ? (
+                <li>
+                <button className='btn' onClick={onLogout}>
+                    <FaSignOutAlt /> Logout 
+                </button>
+                </li>
+            ) : (
+                <>
+                    <li>
+                    <Link to='/login'>
+                        <FaSignInAlt /> Login 
+                    </Link>
+                    </li> 
+                </>
+                )
+            }
+            
         </ul>
     </header>
-  )
+    )
 }
 
 export default Header
